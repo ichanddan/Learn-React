af911821490693fe13e9c1a0f54faf95602cc675
ichanddan <code.us.cm@gmail.com>
2024-03-23 12:55:04 +0530

    Learn Default Props and Props Type

--- src/components/Props/LearnProps.js
+++ src/components/Props/LearnProps.js
@@ -1,22 +1,21 @@
-import React from 'react'
-
-// steps passing props and use props npm i 
+import React from 'react';
+import propTypes from 'prop-types';
 
 // // step 1  =>>>
 // export const LearnProps = (props) => {
-//   return (
-//     <div>
-//         <h1>{props.name}</h1>
-//         <h2>{props.age}</h2>
-//         <h3>{props.address}</h3>
-//     </div>
-//   )
-// }
-
-// // step 2  =>>>
-export const LearnProps = ({name,age,address}) => {
-  return (
-    <div>
+//       return (
+//             <div>
+//                 <h1>{props.name}</h1>
+//                 <h2>{props.age}</h2>
+//                 <h3>{props.address}</h3>
+//             </div>
+//           )
+//         }
+        
+        // // step 2  =>>>
+        export const LearnProps = ({name,age,address}) => {
+            return (
+                <div>
         <h1>{name}</h1>
         <h2>{age}</h2>
         <h3>{address}</h3>
@@ -24,3 +23,22 @@ export const LearnProps = ({name,age,address}) => {
   )
 }
 
+
+// steps props type 
+// steps 1 (npm i prop-types)
+// step 2 define propTypes like string number bollen flot
+
+
+LearnProps.propTypes={
+    name: propTypes.string,
+    age: propTypes.number,
+    address: propTypes.string
+};
+
+//step 3 define default propType if you not deinde props automatice used default props
+
+LearnProps.defaultProps={
+    name: "Name",
+    age: 0,
+    address: "Address"
+}
