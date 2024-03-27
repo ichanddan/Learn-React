80befbc92a99c7cb48456515da5697decfad8d09
ichanddan <code.us.cm@gmail.com>
2024-03-27 23:50:23 +0530

    fix route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { LearnProps } from './components/Props/LearnProps';
-import Navbar from './components/Navbar';
-import { Effect } from './components/Hooks/Effect';
-import Data from './components/Movie/Data';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LearnProps } from "./components/Props/LearnProps";
+import Navbar from "./components/Navbar";
+import { Effect } from "./components/Hooks/Effect";
+import Data from "./components/Movie/Data";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<LearnProps name="Chandan Kumar Maurya" age={44} address="Pipari Rayan Bhadohi"/>} />
-        <Route path="/effect" element={<Effect/>} />
-        <Route path="/movie" element={<Data/>} />
-      </Routes> 
+        <Route
+          path="/"
+          element={
+            <LearnProps
+              name="Chandan Kumar Maurya"
+              age={44}
+              address="Pipari Rayan Bhadohi"
+            />
+          }
+        />
+        <Route path="/effect" element={<Effect />} />
+        <Route path="/movie" element={<Data />} />
+      </Routes>
     </BrowserRouter>
   );
 }
